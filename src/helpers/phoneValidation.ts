25dd821f39be82bd5cb6d7e770be8b2f333f667f
MikleBr <barulin.ma@gmail.com>
2022-08-25 16:21:59 +0300

    Доработал валидацию телефона

--- src/helpers/phoneValidation.ts
+++ src/helpers/phoneValidation.ts
@@ -1,12 +1,12 @@
 import { ValidationResult } from './../@types/entities/ValidationResult';
 
-let PHONE_REGEX = /^(8)[0-9]{10}/;
+let PHONE_REGEX = /^((\+7)|8)\d{10}$/;
 
 export function validatePhoneNumber(phoneNumber: string): ValidationResult {
   if (!PHONE_REGEX.test(phoneNumber)) {
     return {
       status: 'error',
-      message: 'Введите телефон в фермате 89999999999',
+      message: 'Введите телефон в формате +79999999999',
     };
   } else {
     return {
