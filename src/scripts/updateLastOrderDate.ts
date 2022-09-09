7a6d07313b2a19b0519c1118abc15da0f7b984a9
MikleBr <barulin.ma@gmail.com>
2022-09-09 12:56:44 +0300

    feat: скрипт обновления lastOrderDate

--- src/scripts/updateLastOrderDate.ts
+++ src/scripts/updateLastOrderDate.ts
@@ -1,46 +1,42 @@
-import { differenceInDays } from 'date-fns';
-import { Telegram } from 'telegraf';
 import { iikoApi } from '../api/iikoApi';
 import { User } from '../common/entities/User';
+import { interviewService } from '../common/services/interview.service';
 import { userService } from '../common/services/user.service';
 import { AppDataSource } from '../database/appDataSourse';
-
-type Congratulation = {
-  user: User;
-  text: string;
-};
 
 const token = process.env.USER_BOT_TOKEN;
 if (!token) {
   throw new Error('BOT_TOKEN must be provided!');
 }
 
-const telegram = new Telegram(token);
-
 async function updateUsersBalance() {
   const users = await userService.getAll({ step: 'registered' });
 
-  const updatedUserPromises = users.map(async user => {
+  for (const user of users) {
     await checkUserBalance(user);
-  });
-
-  Promise.all(updatedUserPromises);
+  }
 }
 
 async function checkUserBalance(user: User) {
   const currentBalance = user.balance;
-  const iikoBalance = await iikoApi.getUserBalance(user.iikoId);
+  try {
+    const iikoBalance = await iikoApi.getUserBalance(user.iikoId);
+    if (currentBalance === iikoBalance) return;
 
-  if (currentBalance === iikoBalance) return;
+    console.log(
+      `Update order date for ${user.firstName} ${user.secondName} - ${user.telegramId}`
+    );
 
-  console.log(
-    `Update order date for ${user.firstName} ${user.secondName} - ${user.telegramId}`
-  );
+    await interviewService.create(user.id);
 
-  return await userService.update(user.telegramId, {
-    balance: iikoBalance,
-    lastOrderDate: new Date(),
-  });
+    return await userService.update(user.telegramId, {
+      balance: iikoBalance,
+      lastOrderDate: new Date(),
+    });
+  } catch (error) {
+    console.log(`Error on user ${user.telegramId}`);
+    return;
+  }
 }
 
 async function bootstrap() {
