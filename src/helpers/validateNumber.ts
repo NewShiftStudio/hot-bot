ef5488528549134bfa5ba05f951906d1debf7b9f
Mikhail Barulin <87764644+MikleNewShift@users.noreply.github.com>
2022-09-10 15:14:42 +0300

    Feat interview logic 361 (#9)
    * fix: user registration balance
    
    * fix: send mailing logic
    
    * feat: логика создания опроса для пользователя
    
    * feat: cancel interview
    
    * feat: валидация оценки в interview
    
    Co-authored-by: MikleBr <barulin.ma@gmail.com>

---
+++ src/helpers/validateNumber.ts
@@ -0,0 +1,27 @@
+import { ValidationResult } from '../@types/entities/ValidationResult';
+
+export function validateNumber(number: number): ValidationResult {
+  if (isNaN(number)) {
+    return {
+      status: 'error',
+      message: 'Введите число!',
+    };
+  }
+
+  if (number < 1)
+    return {
+      status: 'error',
+      message: 'Ну не все так плохо! Должно быть не меньше 1',
+    };
+
+  if (number > 10)
+    return {
+      status: 'error',
+      message: 'Мы понимаем как вам понравилось, но 10 хватит!',
+    };
+
+  return {
+    status: 'success',
+    message: '',
+  };
+}
