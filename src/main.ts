b3190dbb20eb2a5fdc22a97e0c68c83513a49cc5
MikleBr <barulin.ma@gmail.com>
2022-09-14 17:01:51 +0300

    feat: rework env logic

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,38 @@
 import { AppDataSource } from './database/appDataSourse';
 import { bot as userBot } from './bot/bot';
 import { app } from './server/server';
+import { getRequiredEnvsByNodeEnv } from './helpers/gerRequiredEnvsByNodeEnv';
+import type { NodeEnv } from './@types/entities/App';
+
+const envs = [
+  'NODE_ENV',
+  'PUBLIC_FOLDER',
+  'DB_HOST',
+  'DB_PORT',
+  'DB_USERNAME',
+  'DB_PASSWORD',
+  'DB_DATABASE',
+  'IIKO_API_LOGIN',
+  'REGISTER_ADMIN_PASSWORD',
+];
+
+const requiredEnvs = getRequiredEnvsByNodeEnv(
+  { common: envs, development: ['SENTRY_DSN'], production: ['SENTRY_DSN'] },
+  process.env.NODE_ENV as NodeEnv,
+);
+
+requiredEnvs.forEach((envKey) => {
+  if (!process.env[envKey]) {
+    throw new Error(`Added ${envKey} to .env file !!`);
+  }
+});
 
 export async function bootstrap() {
   try {
     await AppDataSource.initialize();
     console.log('Data Source has been initialized!');
 
-    if (process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'local') {
       await userBot.launch();
       console.log('Bot successfully launched!');
     } else {
